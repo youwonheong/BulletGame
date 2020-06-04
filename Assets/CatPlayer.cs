bb79848cd94e6f04c79921cc2c23e94ea60c6502
soorichu <soorichu@gmail.com>
2020-06-04 16:19:04 +0900

    6월 4일(정수영 실습)

---
+++ Assets/CatPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPlayer : MonoBehaviour
+{
+    Rigidbody rigid;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Debug.Log("앞으로~");
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z + 3f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("뒤로~");
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z - 3f * Time.deltaTime);
+        }
+    }
+}
