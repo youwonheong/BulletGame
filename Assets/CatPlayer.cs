d2e6038bdfc87ed7e900fcc7f41e240cc5b59d01
youwonheong <49299979+youwonheong@users.noreply.github.com>
2020-06-10 15:13:36 +0900

    20020610 30210 유원형

--- Assets/CatPlayer.cs
+++ Assets/CatPlayer.cs
@@ -4,29 +4,86 @@ using UnityEngine;
 
 public class CatPlayer : MonoBehaviour
 {
-    Rigidbody rigid;
+    // Start is called before the first frame update
     void Start()
     {
-        rigid = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow)) {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
             Debug.Log("앞으로~");
             transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y,
-                transform.position.z + 3f * Time.deltaTime);
+                            transform.position.x,
+                            transform.position.y,
+                            transform.position.z + 3f * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             Debug.Log("뒤로~");
             transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y,
-                transform.position.z - 3f * Time.deltaTime);
+                            transform.position.x,
+                            transform.position.y,
+                            transform.position.z - 3f * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("오른쪽으로~");
+            transform.position = new Vector3(
+                            transform.position.x + 3f * Time.deltaTime,
+                            transform.position.y,
+                            transform.position.z );
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("왼쪽으로~");
+            transform.position = new Vector3(
+                            transform.position.x - 3f * Time.deltaTime,
+                            transform.position.y,
+                            transform.position.z);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
